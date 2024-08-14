693b01c080565362bd166c4f9993521dc57db619
Dmitriy Semushin <semu-dima@yandex.ru>
2024-08-14 22:43:43 +0300

    add entities

---
+++ BagXML.DAL/Entities/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace BagXML.DAL.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}
---
+++ BagXML.DAL/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace BagXML.DAL.Entities
+{
+    public sealed class User : BaseEntity
+    {
+        public string FIO { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
