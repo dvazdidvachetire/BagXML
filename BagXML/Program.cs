ccf115fad4da0edcb262fd085a0b47dd90af7469
Dmitriy Semushin <semu-dima@yandex.ru>
2024-08-14 22:42:51 +0300

    change code

--- BagXML/Program.cs
+++ BagXML/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using BagXML.DTOs;
+using BagXML.Services;
+
+Console.Out.WriteLine("Initialize...");
+
+var fileName = string.Empty;
+
+var opdService = new OpenFileDialogService();
+var serializer = new XMLSerializerService();
+
+var thread = new Thread(() => opdService.OpenFileDialog(out fileName));
+thread.SetApartmentState(ApartmentState.STA);
+thread.Start();
+thread.Join();
+
+if (fileName is { Length: > 0})
+{
+    using var fs = new FileStream(fileName, FileMode.Open);
+        
+    serializer.Deserialize(fs);
+    var orders = serializer.DeserializeObject as Orders;
+}
+
+Console.Out.WriteLine("Done!");
+Console.In.ReadLine();
