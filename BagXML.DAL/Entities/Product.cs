d90a43f614192bf74bb7e56c11a44994586e3f7a
Dmitriy Semushin <semu-dima@yandex.ru>
2024-08-16 21:23:02 +0300

    delete entities

--- BagXML.DAL/Entities/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BagXML.DAL.Entities
-{
-    public sealed class Product : BaseEntity
-    {
-        public int Quantity { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public decimal Price { get; set; }
-    }
-}
--- BagXML.DAL/Entities/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BagXML.DAL.Entities
-{
-    public sealed class User : BaseEntity
-    {
-        public string FIO { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-    }
-}
