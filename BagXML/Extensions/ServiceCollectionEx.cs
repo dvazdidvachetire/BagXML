2be87e6de876ee8c296fc289c7a8ae27e80e3cce
Dmitriy Semushin <semu-dima@yandex.ru>
2024-08-16 17:21:15 +0300

    add extension

---
+++ BagXML/Extensions/ServiceCollectionEx.cs
@@ -0,0 +1,29 @@
+﻿using BagXML.Queries;
+using BagXML.Services;
+using System.Data;
+using BagXML.DAL.Repositories.Implementations;
+using BagXML.DAL.Repositories.Interfaces;
+using System.Data.SQLite;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionEx
+    {
+        /// <summary>Расширение для IServiceCollection</summary>
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IDbConnection>(_ => new SQLiteConnection("Data Source=shop.db;"));
+
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            services.AddScoped<OpenFileDialogService>();
+            services.AddScoped<XMLSerializerService>();
+                        
+            services.AddScoped<OrderRepository>();
+            services.AddScoped<ProductRepository>();
+            services.AddScoped<UserQueries>();
+        }
+    }
+}
