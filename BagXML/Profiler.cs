a471be4a6138ab5543b45d0394a571cd6f7909b5
Dmitriy Semushin <semu-dima@yandex.ru>
2024-08-17 00:41:31 +0300

    add profiler

---
+++ BagXML/Profiler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BagXML.DAL.Entities;
+using BagXML.Models;
+
+namespace BagXML
+{
+    public sealed class Profiler : Profile
+    {
+        public Profiler()
+        {
+            CreateMap<Product, ProductEntity>()
+                .ForMember(dest => dest.Price, opt => opt.MapFrom(s => decimal.Parse(s.Price.Replace('.', ','))))
+                .ForMember(dest => dest.Quantity, opt => opt.MapFrom(s => int.Parse(s.Quantity)));
+            CreateMap<Product, Product>();
+            CreateMap<ProductOrder, ProductOrderEntity>();
+            CreateMap<User, UserEntity>();
+        }
+    }
+}
