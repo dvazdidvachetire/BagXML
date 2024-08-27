79b6978dd6e77e479abd0575f134c536e0fe14c9
Dmitriy Semushin <semu-dima@yandex.ru>
2024-08-27 21:54:05 +0300

    modified code queries

--- BagXML/Queries/OrderQueries.cs
+++ BagXML/Queries/OrderQueries.cs
@@ -1,53 +1,72 @@
 ﻿using AutoMapper;
 using BagXML.DAL.Entities;
-using BagXML.DAL.Repositories.Interfaces;
+using BagXML.DAL.Repositories.Implementations;
+using BagXML.DAL.UnitOfWork;
 using BagXML.Models;
+using System.Data;
+using System.Data.SQLite;
 
 namespace BagXML.Queries
 {
     /// <summary>представляет запросы к бд к таблице с заказами</summary>
     public sealed class OrderQueries : Queries<Order>
     {
-        private readonly IOrderRepository _orderRepository;
+        private readonly UnitOfWork _unitOfWork;
         private readonly ProductQueries _productQueries;
         private readonly UserQueries _userQueries;
         private readonly ProductOrderQueries _productOrderQueries;
         private readonly IMapper _mapper;
 
-        public OrderQueries(IOrderRepository orderRepository, 
+        public OrderQueries(UnitOfWork unitOfWork, 
                             ProductQueries productQueries, 
                             UserQueries userQueries,
                             ProductOrderQueries productOrderQueries,
                             IMapper mapper)
         {
-            (_orderRepository, _productQueries, _userQueries, _productOrderQueries, _mapper) 
-            = (orderRepository, productQueries, userQueries, productOrderQueries, mapper);
+            (_unitOfWork, _productQueries, _userQueries, _productOrderQueries, _mapper) 
+            = (unitOfWork, productQueries, userQueries, productOrderQueries, mapper);
         }
 
-        public override int Create(Order model)
+        public override int Create(Order model, IDbTransaction? dbTransaction = null)
         {
-            var productId = 0;
-            var userId = _userQueries.Create(model.User);
-            var orderId = _orderRepository.Create(new OrderEntity
-            {
-                No = int.Parse(model.No),
-                Reg_Date = model.Reg_Date,
-                Sum = double.Parse(model.Sum.Replace('.', ',')),
-                UserId = userId,
-            });
+            dbTransaction = _unitOfWork.DBConnection.BeginTransaction();
 
-            foreach (var product in model.Products)
+            try
             {
-                productId = _productQueries.Create(_mapper.Map<Product>(product));
-
-                _productOrderQueries.Create(new ProductOrder 
+                var productId = 0;
+                var userId = _userQueries.Create(model.User, dbTransaction);
+                var orderRepo = _unitOfWork.GetRepository(new OrderRepository(_unitOfWork.DBConnection));
+                var orderId = orderRepo.Create(new OrderEntity
                 {
-                    OrderId = orderId,
-                    ProductId = productId
-                });
-            }
+                    No = int.Parse(model.No),
+                    Reg_Date = model.Reg_Date,
+                    Sum = double.Parse(model.Sum.Replace('.', ',')),
+                    UserId = userId,
+                }, dbTransaction);
 
-            return orderId;
+                foreach (var product in model.Products)
+                {
+                    productId = _productQueries.Create(_mapper.Map<Product>(product), dbTransaction);
+
+                    _productOrderQueries.Create(new ProductOrder
+                    {
+                        OrderId = orderId,
+                        ProductId = productId
+                    }, dbTransaction);
+                }
+
+                dbTransaction.Commit();
+
+                return orderId;
+            }
+            catch (SQLiteException ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+
+                dbTransaction.Rollback();
+
+                throw;
+            }
         }
     }
 }
